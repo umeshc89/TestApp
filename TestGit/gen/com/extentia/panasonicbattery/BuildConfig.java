a68cd8b37f626119a6e5b2629d9a4e47bb66d3ec
Umesh Chougule <umesh.chougule@extentia.com>
2014-06-04 14:55:15 +0530

    - testgit

---
+++ TestGit/gen/com/extentia/panasonicbattery/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.extentia.panasonicbattery;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ TestGit/src/com/extentia/panasonicbattery/MainActivity.java
@@ -0,0 +1,25 @@
+package com.extentia.panasonicbattery;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		Toast.makeText(getApplicationContext(), "Hardy ----", Toast.LENGTH_LONG).show();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
